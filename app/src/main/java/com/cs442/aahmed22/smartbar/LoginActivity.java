e844e88d058ec6bbaae536c5e9937986f518964c
Adeeb Ahmed <adeeb.ahmed1995@gmail.com>
2016-10-21 16:05:55 -0500

    Initial commit;

---
+++ app/src/main/java/com/cs442/aahmed22/smartbar/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.cs442.aahmed22.smartbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
