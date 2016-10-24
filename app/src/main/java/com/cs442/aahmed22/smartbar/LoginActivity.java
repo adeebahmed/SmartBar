b6b4ed584ee993d4e04cdcee6700ba3b6d07516d
Adeeb Ahmed <adeeb.ahmed1995@gmail.com>
2016-10-23 20:43:26 -0500

    Added login page;

--- app/src/main/java/com/cs442/aahmed22/smartbar/LoginActivity.java
+++ app/src/main/java/com/cs442/aahmed22/smartbar/LoginActivity.java
@@ -9,5 +9,7 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
     }
 }
--- app/src/main/java/com/cs442/aahmed22/smartbar/MainActivity.java
+++ app/src/main/java/com/cs442/aahmed22/smartbar/MainActivity.java
@@ -1,7 +1,10 @@
 package com.cs442.aahmed22.smartbar;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSignUp = (Button) findViewById(R.id.btnSignUp);
+        final Intent loginIntent = new Intent(this, LoginActivity.class);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(loginIntent);
+            }
+        });
+
     }
 }
