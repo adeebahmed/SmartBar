bd8ec582069ff73ff468788463918a67c8201b79
Adeeb Ahmed <adeeb.ahmed1995@gmail.com>
2016-11-13 10:11:21 -0600

    Adding Login functionality; Added database;

--- app/src/main/java/com/cs442/team2/smartbar/LoginActivity.java
+++ app/src/main/java/com/cs442/team2/smartbar/LoginActivity.java
@@ -5,38 +5,62 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.cs442.team2.smartbar.data.DataBaseHelper;
 
 public class LoginActivity extends Activity {
 
+    private String un,pw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-
-        /* ADD FOR UPDATING FRIENDS LIST
-    final ArrayList<FrontDetails> resultse = new ArrayList<FrontDetails>();
-    FrontListBaseAdapter asdf = new FrontListBaseAdapter(context, resultse);
-    lv1.setAdapter(new FrontListBaseAdapter(Front.this, resultse));
-    lv1.setOnItemClickListener(new OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> arg0, View arg1,
-        int position, long arg3) {
-            Object o = lv1.getItemAtPosition(position);
-            FrontDetails obj_itemDetails = (FrontDetails)o;
-            Toast.makeText(context, "You have chosen " + ' ' + obj_itemDetails.getMsgType(), Toast.LENGTH_LONG).show();
-        }
-    });
-    */
+//        /* ADD FOR UPDATING FRIENDS LIST
+//    final ArrayList<FrontDetails> resultse = new ArrayList<FrontDetails>();
+//    FrontListBaseAdapter asdf = new FrontListBaseAdapter(context, resultse);
+//    lv1.setAdapter(new FrontListBaseAdapter(Front.this, resultse));
+//    lv1.setOnItemClickListener(new OnItemClickListener() {
+//        @Override
+//        public void onItemClick(AdapterView<?> arg0, View arg1,
+//        int position, long arg3) {
+//            Object o = lv1.getItemAtPosition(position);
+//            FrontDetails obj_itemDetails = (FrontDetails)o;
+//            Toast.makeText(context, "You have chosen " + ' ' + obj_itemDetails.getMsgType(), Toast.LENGTH_LONG).show();
+//        }
+//    });
+//    */
 
         Button btnLogIn = (Button) findViewById(R.id.btn_login);
+        un = ((TextView) findViewById(R.id.input_email)).getText().toString();
+        pw = ((TextView) findViewById(R.id.input_password)).getText().toString();
+
         final Intent profileIntent = new Intent(this, ProfileActivity.class);
 
         btnLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(profileIntent);
+                if(user_exists(un,pw)){
+                    profileIntent.putExtra("username", un);
+                    startActivity(profileIntent);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Invalid username or password.", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
+    }
+
+    private boolean user_exists(String u, String p){
+        //database racked locally
+        DataBaseHelper dbh = new DataBaseHelper(getApplicationContext());
+        try {
+            dbh.openDataBase();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dbh.userExists(u,p);
     }
 }
