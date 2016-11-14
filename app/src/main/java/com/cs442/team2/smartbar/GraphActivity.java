7cfc677b621798bd851059c714a256099b92bbea
Sumedha Gupta <sumedha.gupta22@gmail.com>
2016-11-13 20:22:22 -0600

    Prasanna files: Profile screen changed, added some attributes in users table in db

--- app/src/main/java/com/cs442/team2/smartbar/GraphActivity.java
+++ app/src/main/java/com/cs442/team2/smartbar/GraphActivity.java
@@ -1,14 +1,17 @@
 package com.cs442.team2.smartbar;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
 
-import java.util.Calendar;
-import java.util.Date;
+import com.cs442.team2.smartbar.data.DataBaseHelper;
 import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.io.IOException;
 
 
 /**
@@ -16,37 +19,90 @@ import com.jjoe64.graphview.series.LineGraphSeries;
  */
 
 public class GraphActivity extends Activity {
+    DataBaseHelper wDbHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
-        Calendar calendar = Calendar.getInstance();
-        Date d1 = calendar.getTime();
-        calendar.add(Calendar.DATE, 1);
-        Date d2 = calendar.getTime();
-        calendar.add(Calendar.DATE, 1);
-        Date d3 = calendar.getTime();
+        Bundle bundle = getIntent().getExtras();
+        int month = bundle.getInt("month");
+        month++;
+        int day = bundle.getInt("day");
+        int tday = day;
+        int year = bundle.getInt("year");
+        int userid= bundle.getInt("userid");
+
+        double[] x = {0,0,0};
+        int i=0;
+
+        wDbHelper = new DataBaseHelper(this);
+        try {
+
+            wDbHelper.createDataBase();
+
+        } catch (IOException ioe) {
+
+            throw new Error("Unable to create database");
+
+        }
+        try {
+
+            wDbHelper.openDataBase();
+
+        } catch (Exception sqle) {
+
+            //throw sqle;
+
+        }
+        while(i<3) {
+            String sql = "select * from workouts where date like '%"+year+"-"+month+"-"+tday+"%' and fk_user_id="+userid;
+            Cursor cursor = wDbHelper.getReadableDatabase().rawQuery(sql, null);
+            while (cursor.moveToNext())
+
+            {
+
+                int sets = cursor.getInt(cursor.getColumnIndex("sets"));
+                int reps = cursor.getInt(cursor.getColumnIndex("reps"));
+                int weight = cursor.getInt(cursor.getColumnIndex("weight"));
+                double temp = ((double)weight * (double)sets * (double)reps) / (double)1000;
+                x[i] = temp;
+
+            }
+            tday++;
+            i++;
+        }
+        String date1 = Integer.toString(month)+"/"+Integer.toString(day)+"/"+Integer.toString(year);
+        String date2 = Integer.toString(month)+"/"+Integer.toString(day+1)+"/"+Integer.toString(year);
+        String date3 = Integer.toString(month)+"/"+Integer.toString(day+2)+"/"+Integer.toString(year);
+
+
 
         GraphView graph = (GraphView) findViewById(R.id.graph);
 
 
 
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(d1, 1),
-                new DataPoint(d2, 5),
-                new DataPoint(d3, 3)
+                new DataPoint(0, x[0]),
+                new DataPoint(1, x[1]),
+                new DataPoint(2, x[2])
         });
 
         graph.addSeries(series);
+        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
+        staticLabelsFormatter.setHorizontalLabels(new String[] {date1,date2,date3});
 
 
-        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(GraphActivity.this));
+        graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
         graph.getGridLabelRenderer().setNumHorizontalLabels(3); // only 4 because of the space
 
 
-        graph.getViewport().setMinX(d1.getTime());
-        graph.getViewport().setMaxX(d3.getTime());
-        graph.getViewport().setXAxisBoundsManual(true);
+
+
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinY(1);
+        graph.getViewport().setMaxY(5);
+        series.setDrawBackground(true);
+        series.setBackgroundColor(Color.BLUE);
 
 
         graph.getGridLabelRenderer().setHumanRounding(false);
--- app/src/main/java/com/cs442/team2/smartbar/ProfileActivity.java
+++ app/src/main/java/com/cs442/team2/smartbar/ProfileActivity.java
@@ -1,32 +1,118 @@
 package com.cs442.team2.smartbar;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.cs442.team2.smartbar.data.DataBaseHelper;
+
+import java.io.IOException;
 
 public class ProfileActivity extends AppCompatActivity
 {
+    DataBaseHelper wDbHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        Log.d("bye","bye");
+        String s= "Prasana";
+        wDbHelper = new DataBaseHelper(this);
+        try {
 
-        /*added by Sumedha Gupta
-        *adding button listeners to navigate to other screens
-        * */
-        //listener for Journal
-        Button userJournalButton= (Button) findViewById(R.id.journal);
-        userJournalButton.setOnClickListener(new View.OnClickListener()
+            wDbHelper.createDataBase();
+
+        } catch (IOException ioe) {
+
+            throw new Error("Unable to create database");
+
+        }
+        try {
+
+            wDbHelper.openDataBase();
+
+        } catch (Exception sqle) {
+
+            //throw sqle;
+
+        }
+
+        String sql = "select * from users where first_name like '%"+s+"%'";
+        Cursor cursor = wDbHelper.getReadableDatabase().rawQuery(sql, null);
+
+        while (cursor.moveToNext())
+
         {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),UserJournalActivity.class);
-                startActivity(intent);
-            }
-        });
 
+            String firstname= cursor.getString(cursor.getColumnIndex("first_name"));
+            String lastname= cursor.getString(cursor.getColumnIndex("last_name"));
+            int age = cursor.getInt(cursor.getColumnIndex("age"));
+            final int userid = cursor.getInt(cursor.getColumnIndex("_id"));
+            String location= cursor.getString(cursor.getColumnIndex("Location"));
+            String name = firstname+"\t"+lastname;
+            final int weight = cursor.getInt(cursor.getColumnIndex("weight"));
+            int height = cursor.getInt(cursor.getColumnIndex("height"));
+
+            double x = weight * 0.45;
+            double y = height * 0.025;
+            double z= y * y;
+            double bmi = x / z;
+
+
+
+            TextView tname = (TextView)findViewById(R.id.text1);
+            TextView tage = (TextView)findViewById(R.id.text2);
+            TextView tlocation = (TextView)findViewById(R.id.text3);
+            TextView tbmi = (TextView)findViewById(R.id.text4);
+
+
+            tname.setText(name);
+            tage.setText(Integer.toString(age));
+            tlocation.setText(location);
+            tbmi.setText(String.format("%.2f",bmi));
+
+            final Intent logoutIntent = new Intent(this, LoginActivity.class);
+            final Intent journalIntent = new Intent(this, UserJournalActivity.class);
+            final Intent grpahIntent = new Intent(this, PickdateActivity.class);
+            final Intent friendsIntent = new Intent(this, LoginActivity.class);
+            Button btnlogout = (Button)findViewById(R.id.Button06);
+            Button btnjournal = (Button)findViewById(R.id.journal);
+            Button btngraph = (Button)findViewById(R.id.Button02);
+            Button btnfriends = (Button)findViewById(R.id.Button03);
+
+            btnlogout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(logoutIntent);
+                }
+            });
+            btnjournal.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(journalIntent);
+                }
+            });
+            btngraph.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Bundle b = new Bundle();
+                    b.putInt("userid",userid);
+                    grpahIntent.putExtras(b);
+                    startActivity(grpahIntent);
+                }
+            });
+            btnfriends.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(friendsIntent);
+                }
+            });
+        }
 
     }
 }
