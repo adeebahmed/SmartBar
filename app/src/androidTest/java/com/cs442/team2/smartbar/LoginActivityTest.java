77c319116dbbec84dfe24275ee8607e2d528f238
Adeeb Ahmed <adeeb.ahmed1995@gmail.com>
2016-11-27 17:18:58 -0600

    Added Login tests;

--- app/src/androidTest/java/com/cs442/team2/smartbar/LoginActivityTest.java
+++ app/src/androidTest/java/com/cs442/team2/smartbar/LoginActivityTest.java
@@ -6,6 +6,14 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 /**
  * Created by thedeeb on 11/27/16.
  */
@@ -16,28 +24,39 @@ public class LoginActivityTest {
     public ActivityTestRule<LoginActivity> mActivityRule =
             new ActivityTestRule<>(LoginActivity.class);
 
-    @Test
-    public void onCreate() throws Exception {
-
-    }
 
     @Test
     public void successfulLogin(){
+        onView(withId(R.id.input_email)).perform(typeText("adeeb"), closeSoftKeyboard());
+        onView(withId(R.id.input_password)).perform(typeText("smartbar"), closeSoftKeyboard());
+        onView(withText("Login")).perform(click());
+    }
 
+    @Test
+    public void invalidLogin(){
+        onView(withId(R.id.input_email)).perform(typeText("adeeb"), closeSoftKeyboard());
+        onView(withId(R.id.input_password)).perform(typeText("wrongpassword"), closeSoftKeyboard());
+        onView(withText("Login")).perform(click());
     }
 
     @Test
     public void blankLogin(){
-
-    }
-
-    @Test
-    public void nullLogin(){
-
+        onView(withId(R.id.input_email)).perform(typeText(""), closeSoftKeyboard());
+        onView(withId(R.id.input_password)).perform(typeText(""), closeSoftKeyboard());
+        onView(withText("Login")).perform(click());
     }
 
     @Test
     public void invalidCharLogin(){
+        String invalidChars[] = {".","#","$","[","]"};
+
+        for(int i = 0; i < invalidChars.length; i++){
+            onView(withId(R.id.input_email)).perform(typeText(""), closeSoftKeyboard());
+            onView(withId(R.id.input_password)).perform(typeText(""), closeSoftKeyboard());
+
+            onView(withId(R.id.input_email)).perform(typeText(invalidChars[i]), closeSoftKeyboard());
+            onView(withId(R.id.input_password)).perform(typeText(invalidChars[i]), closeSoftKeyboard());
+        }
 
     }
 
